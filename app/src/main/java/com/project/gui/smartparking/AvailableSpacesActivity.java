38787614ef6b343c98ab62f6d18df6583489b7b2
ondermarangoz <ondermarangoz7@gmail.com>
2019-01-02 23:13:45 +0300

    Map Activity handled

--- app/src/main/java/com/project/gui/smartparking/AvailableSpacesActivity.java
+++ app/src/main/java/com/project/gui/smartparking/AvailableSpacesActivity.java
@@ -3,6 +3,7 @@ package com.project.gui.smartparking;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -23,9 +24,14 @@ public class AvailableSpacesActivity extends Activity {
         carImgButton2 = findViewById(R.id.carImgButton2);
         carImgButton3 = findViewById(R.id.carImgButton3);
 
+        carImgButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AvailableSpacesActivity.this, MapsActivity.class);
+                startActivity(intent);
 
-        Intent intent = new Intent(this,MapActivity.class);
-        startActivity(intent);
+            }
+        });
 
 
 
